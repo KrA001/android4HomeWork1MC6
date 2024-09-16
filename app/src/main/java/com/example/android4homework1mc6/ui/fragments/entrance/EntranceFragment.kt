33edf58ef62043a5b9d30bfee6320e9f6423c2bc
LENOVO <test23062000@gmail.com>
2024-09-16 16:13:50 +0300

    домашка c 1 допом 1 домашкой ии

---
+++ app/src/main/java/com/example/android4homework1mc6/ui/fragments/entrance/EntranceFragment.kt
@@ -0,0 +1,17 @@
+package com.example.android4homework1mc6.ui.fragments.entrance
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.android4homework1mc6.R
+import com.example.android4homework1mc6.databinding.FragmentEntranceBinding
+
+class EntranceFragment : Fragment(R.layout.fragment_entrance) {
+
+    private val binding by viewBinding(FragmentEntranceBinding::bind)
+}
+
